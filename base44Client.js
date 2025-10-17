88177e6f8f77867a06b36ea155856c219c64d8a1
Pasumurthy Venu Munendra Kumar <venupasumurthy0509@gmail.com>
2025-10-18 02:45:48 +0530

    Delete base44Client.js

--- base44Client.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@base44/sdk';
-// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
-
-// Create a client with authentication required
-export const base44 = createClient({
-  appId: "68f20b9f302c722f7ad1fdd2", 
-  requiresAuth: true // Ensure authentication is required for all operations
-});
